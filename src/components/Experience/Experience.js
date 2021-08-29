eb8ec1fda842e347f5ee1e156c42f8f1bf226e9c
Cortlando <48654220+Cortlando@users.noreply.github.com>
2021-08-29 18:51:18 -0500

    Changed tab label, and added everything to experience section.
    Just have to add more style to the experience section

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header'
+import { Button } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
 import './Experience.css'
 const Experience = () => {
@@ -35,31 +36,50 @@ const Experience = () => {
 
 
                         <div className="Projects">
-
-
                             <div className="ProjectOne">
-                                <Typography variant="h4" >
-                                    Covid-19 Tracker
-                                </Typography>
-                                <Typography variant="h5">
-                                    My Team and I created a web application that let the user track Covid-19 cases across the world. When looking at cases within the United States, cases could be sorted by state.<br></br>
-                                    The data needed for the site was obtained through various APIs and was displayed with charts and graphs <br></br>
-                                    The application was developed using React.js and Node.js along with Chart.js and Material-Ui
-                                </Typography>
+                                <div className="ProjectOneHeader">
+                                    <Typography variant="h4" >
+                                        Covid-19 Tracker
+                                    </Typography>
+                                </div>
+                                <div className="ProjectOneImg">
+                                    <img id="ProjectOneImg" src={process.env.PUBLIC_URL + '/ProjectOneImage.png'} alt="Image of Covid-19Tracker" />
+                                </div>
+                                <div className="ProjectOneText">
+                                    <Typography variant="h5">
+                                        My Team and I created a web application that let the user track Covid-19 cases across the world. When looking at cases within the United States, cases could be sorted by state.<br></br>
+                                        The data needed for the site was obtained through various APIs and was displayed with charts and graphs <br></br>
+                                        The application was developed using React.js and Node.js along with Chart.js and Material-Ui
+                                    </Typography>
+                                </div>
+                                <div className="ProjectOneButtons">
+                                    <div className="ProjectOneRepo">
+                                        <Button href="https://github.com/Cortlando/Covid-19-Tracker" variant="contained" color="secondary" size="large">
+                                            Repo
+                                        </Button>
+                                    </div>
+                                </div>
+
                             </div>
 
-
-
                             <div className="ProjectTwo">
-                                <Typography variant="h4">
-                                    Intern Database Manager
-                                </Typography>
-                                <Typography variant="h5">
-                                    This application was made to allow a user to quickly view and edit a database containg the information of every Discover intern in the Code_Orange program. <br></br>
-                                    Users could add interns to the database,  edit information about interns(email, phone number, etc...), and remove them from the database <br></br>
-                                    The Interns in the database would be organized into tables in the app, and users could sort through them in various ways.(alphabetically, by location, amount of years in program, etc...)<br></br>
-                                    This application was developed using React.js and SpringBoot
-                                </Typography>
+                                <div className="ProjectTwoHeader">
+                                    <Typography variant="h4">
+                                        Intern Database Manager
+                                    </Typography>
+                                </div>
+                                <div className="ProjectTwoImg">
+                                    <img id="ProjectTwoImg" src={process.env.PUBLIC_URL + '/ProjectTwoImage.png'} alt="Image of Intern Database Manager"/>
+                                </div>
+
+                                <div className="ProjectTwoText">
+                                    <Typography variant="h5">
+                                        This application was made to allow a user to quickly view and edit a database containg the information of every Discover intern in the Code_Orange program. <br></br>
+                                        Users could add interns to the database,  edit information about interns(email, phone number, etc...), and remove them from the database <br></br>
+                                        The Interns in the database would be organized into tables in the app, and users could sort through them in various ways.(alphabetically, by location, amount of years in program, etc...)<br></br>
+                                        This application was developed using React.js and SpringBoot
+                                    </Typography>
+                                </div>
                             </div>
                         </div>
                     </div>
